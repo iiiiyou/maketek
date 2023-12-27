b6439ca40ad28131bb49d25cb8f0f1b9f2a054d2
KY <clark0701@gmail.com>
2023-12-28 08:53:47 +0900

    20231228-002

--- predict_camera.py
+++ predict_camera.py
@@ -14,12 +14,37 @@ def makedirs(path):
         print("Error: Failed to create the directory.")
 
 
+#count
+def count_fire(detected_list):
+
+    if len(detected_list) > 10:
+        detected_list.pop(0)  # Remove the first element
+        
+
+        if detected_list.count(1) > 3:
+            
+            cords = result.boxes.xyxy[0].tolist()
+            cords = [round(x) for x in cords]
+            start = cords[0:2]  # x1,y1
+
+            start.insert(0,1)
+
+            print("-----------------------")
+            print(start)
+            
+            # Saving images
+            cv2.imwrite('C:/Workplace/i4u/maketek/detect_image/'+date.format_date()+'/'+date.get_time_in_mmddss()+'.jpg', imS)
+
+            modbus.write_detected(start)
+
+
+
 # Load the YOLOv8 model
-model = YOLO('model\\best.pt')  # pretrained YOLOv8n model
+model = YOLO('C:/Workplace/i4u/maketek/yolov8n.pt')  # pretrained YOLOv8n model
         
 
 # Open the video file
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # Loop through the video frames
 while cap.isOpened():
@@ -44,28 +69,28 @@ while cap.isOpened():
         # cv2.imshow("YOLOv8 Inference", annotated_frame)
 
         # Make folders if not exsist
-        path='C:\\workspace\\code\\detect_image\\'+date.format_date()+'\\'
+        path='C:/Workplace/i4u/maketek/detect_image/'+date.format_date()+'/'
         makedirs(path)
 
         # Saving images
         # cv2.imwrite('detect_images\\'+date.format_date()+'\\'+date.get_time_in_mmddss()+'.jpg', annotated_frame)
 
+
+        # Create a list to count fire occurances
+        detected_list = []
+
+        
         # Modbus write
         if(len(result.boxes)!=0):
-            cords = result.boxes.xyxy[0].tolist()
-            cords = [round(x) for x in cords]
-            start = cords[0:2]  # x1,y1
-
-            start.insert(0,1)
-
-            print("-----------------------")
-            print(start)
-
-            modbus.write_detected(start)
+            detected_list.append(1)  # Add the last element
         else:
             # Break the loop if the end of the video is reached
+            detected_list.append(0)
             print('no detacted')
             modbus.write_detected([0,0,0])
+
+        print(detected_list)
+        count_fire(detected_list)
 
         # Break the loop if 'q' is pressed
         if cv2.waitKey(1) & 0xFF == ord("q"):
