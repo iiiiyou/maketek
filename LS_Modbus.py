452f30242940c38057011f85779dd5cb2a602877
maketk <maktek@i4u.dev>
2023-12-28 10:07:00 +0900

    20231228-005

--- LS_Modbus.py
+++ LS_Modbus.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:ultralytics/LS_Modbus.py
+# <<<<<<< HEAD:ultralytics/LS_Modbus.py
 from pymodbus.client import ModbusTcpClient
 import pymodbus
 import time
@@ -41,47 +41,4 @@ def write_detected(values):
 
 # # Write 1 to register 0
 # write_detected([1,x,y])
-=======
-from pymodbus.client import ModbusTcpClient
-import pymodbus
-import time
-import interpolate as inter
-
-
-# Example usage:
-
-def write_detected(values):
-  """Writes multiple registers to the PLC.
-
-  Args:
-    client: The Pymodbus client object.
-    address: The address of the first register to write to.
-    values: A list of values to write to the registers.
-
-  Returns:
-    True if the write was successful, False otherwise.
-  """
-
-  try:
-    val = inter.alignment(values)
-    print('----------------')
-    print(val)
-    client = ModbusTcpClient("192.168.1.2", port=502)
-    return1 = client.write_registers(0x0009,val,1)
-    client.close()
-    print("modbus--------------sss")
-    print(return1)
-    print("modbusend---------------")
-    return True
-  except Exception as e:
-    print(e)
-    return False
   
-
-
-# x=500
-# y=1000
-
-# # Write 1 to register 0
-# write_detected([1,x,y])
->>>>>>> b6439ca40ad28131bb49d25cb8f0f1b9f2a054d2:LS_Modbus.py
