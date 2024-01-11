5295c147ca65d0b0ef4c5c2cafacef56d3ea433c
had <astroeye@hotmail.com>
2024-01-11 11:03:44 +0900

    20240111_001

--- train.py
+++ train.py
@@ -4,14 +4,15 @@ import shutil
 
 from roboflow import Roboflow
 
-if not os.path.exists("datasets\\2048_two_class_full_annotation-2"):
+if not os.path.exists("datasets\\2048_two_class_full_annotation-3"):
 
     from roboflow import Roboflow
     rf = Roboflow(api_key="bxW7hooY5jiknZS3GIxI")
     project = rf.workspace("i4umaket").project("2048_two_class_full_annotation")
-    dataset = project.version(2).download("yolov8")
+    dataset = project.version(3).download("yolov8")
 
-    shutil.move("2048_two_class_full_annotation-2", "datasets\\2048_two_class_full_annotation-2\\")
+
+    shutil.move("2048_two_class_full_annotation-3", "datasets\\2048_two_class_full_annotation-3\\")
 
 # 모델을 로드하세요..
 model = YOLO('yolov8m.yaml')  # YAML에서 새 모델 구축
@@ -25,9 +26,9 @@ if __name__ == '__main__':
     print("training start")
     # data.yaml안에 있는 경로 기본 설정 "C:\Users\<user>\AppData\Roaming\Ultralytics\settings.yaml"
     # datasets_dir: C:\workspace\maketek "in settings.yaml"
-    results = model.train(data='datasets\\2048_two_class_full_annotation-2\\data.yaml', epochs=200, imgsz=640)
+    results = model.train(data='datasets\\2048_two_class_full_annotation-3\\data.yaml', epochs=200, imgsz=640)
 
     # data.yaml 안의 image파일 경로
     # test: test/images
     # train: train/images
-    # val: valid/images
+    # val: valid/images 
