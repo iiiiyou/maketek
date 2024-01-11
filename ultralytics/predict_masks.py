8f3bd8a2a49bfe9dd038dc558d89244312d62218
had <astroeye@hotmail.com>
2024-01-12 00:28:23 +0900

    20240112-002

--- ultralytics/predict_masks.py
+++ ultralytics/predict_masks.py
@@ -3,16 +3,16 @@ from ultralytics import YOLO
 # Load a model
 # model = YOLO('C:\\workspace\\maketek\\runs\\detect\\train3\\weights\\best.pt')  # pretrained YOLOv8n model
 # model = YOLO('model\\two_class_full_best_seg_4.pt')  # pretrained YOLOv8n model
-model = YOLO('model\\two_class_full_best_detect_4.pt')  # pretrained YOLOv8n model
+model = YOLO('model\\2048_two_class_full_annotation-5_seg_9.pt')  # pretrained YOLOv8n model
 
-images = ['images\\deform_spot_142804.jpg', \
-                 'images\\raw_151651.jpg']
+images = ['images\\2048_good_and_defective_01.jpg', \
+                 'images\\2048_good_and_defective_02.jpg']
 
 # Run batched inference on a list of images
 results = model(images, stream=True)  # return a generator of Results objects
 
 # Run inference on 'bus.jpg' with arguments
-results = model.predict(images, save=True, imgsz=1080, conf=0.94)
+results = model.predict(images, save=True, imgsz=2048, conf=0.50)
 
 # Process results generator
 if __name__ == '__main__':
