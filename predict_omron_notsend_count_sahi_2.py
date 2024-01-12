d92bfbfcc0d448006cc32d6e2b4d5911789aa840
had <astroeye@hotmail.com>
2024-01-12 15:22:29 +0900

    20240112-008

--- predict_omron_notsend_count_sahi_2.py
+++ predict_omron_notsend_count_sahi_2.py
@@ -72,7 +72,7 @@ detected_list = []
 
 
 # Load the YOLOv8 model
-model = YOLO('models\2048_two_class_full_annotation-5_seg_9.pt')  # pretrained YOLOv8n model
+model = YOLO('models\\2048_two_class_full_annotation-5_seg_9.pt')  # pretrained YOLOv8n model
 # model = YOLO('model\\2048_two_class_full_annotation-2_detect.pt')  # pretrained YOLOv8n model
 
 yolov8_model_path = "models\2048_two_class_full_annotation-5_seg_9.pt"
