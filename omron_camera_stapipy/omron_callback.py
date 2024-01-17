654dc33e775c6d31fc9d99b01a7b1ffb445c4b19
maketk <maktek@i4u.dev>
2024-01-17 15:00:04 +0900

    20240117-001

---
+++ omron_camera_stapipy/omron_callback.py
@@ -0,0 +1,101 @@
+"""
+ This sample shows how to use OpenCV for format conversion and display.
+ The following points will be demonstrated in this sample code:
+ - Initialize StApi
+ - Connect to camera
+ - Register and use callback function with StApi
+ - Acquire image data via callback class function
+ - Copy image data for OpenCV
+ - Convert Bayer image format to RGB using OpenCV
+ - Preview image using OpenCV
+ Note: opencv-python and numpy packages are required:
+    pip install numpy
+    pip install opencv-python
+"""
+
+import cv2
+import threading
+import numpy as np
+import stapipy as st
+
+# Image scale when displaying using OpenCV.
+DISPLAY_RESIZE_FACTOR = 1
+
+
+class CMyCallback:
+    """
+    Class that contains a callback function.
+    """
+
+    def __init__(self):
+        self._image = None
+        self._lock = threading.Lock()
+
+    @property
+    def image(self):
+        duplicate = None
+        self._lock.acquire()
+        if self._image is not None:
+            duplicate = self._image.copy()
+        self._lock.release()
+        return duplicate
+
+    def datastream_callback(self, handle=None, context=None):
+        """
+        Callback to handle events from DataStream.
+
+        :param handle: handle that trigger the callback.
+        :param context: user data passed on during callback registration.
+        """
+        st_datastream = handle.module
+        if st_datastream:
+            with st_datastream.retrieve_buffer() as st_buffer:
+                # Check if the acquired data contains image data.
+                if st_buffer.info.is_image_present:
+                    # Create an image object.
+                    st_image = st_buffer.get_image()
+
+                    # Check the pixelformat of the input image.
+                    pixel_format = st_image.pixel_format
+                    pixel_format_info = st.get_pixel_format_info(pixel_format)
+
+                    # Only mono or bayer is processed.
+                    if not(pixel_format_info.is_mono or
+                           pixel_format_info.is_bayer):
+                        return
+
+                    # Get raw image data.
+                    data = st_image.get_image_data()
+
+                    # Perform pixel value scaling if each pixel component is
+                    # larger than 8bit. Example: 10bit Bayer/Mono, 12bit, etc.
+                    if pixel_format_info.each_component_total_bit_count > 8:
+                        nparr = np.frombuffer(data, np.uint16)
+                        division = pow(2, pixel_format_info
+                                       .each_component_valid_bit_count - 8)
+                        nparr = (nparr / division).astype('uint8')
+                    else:
+                        nparr = np.frombuffer(data, np.uint8)
+
+                    # Process image for display.
+                    nparr = nparr.reshape(st_image.height, st_image.width, 1)
+
+                    # Perform color conversion for Bayer.
+                    if pixel_format_info.is_bayer:
+                        bayer_type = pixel_format_info.get_pixel_color_filter()
+                        if bayer_type == st.EStPixelColorFilter.BayerRG:
+                            nparr = cv2.cvtColor(nparr, cv2.COLOR_BAYER_RG2RGB)
+                        elif bayer_type == st.EStPixelColorFilter.BayerGR:
+                            nparr = cv2.cvtColor(nparr, cv2.COLOR_BAYER_GR2RGB)
+                        elif bayer_type == st.EStPixelColorFilter.BayerGB:
+                            nparr = cv2.cvtColor(nparr, cv2.COLOR_BAYER_GB2RGB)
+                        elif bayer_type == st.EStPixelColorFilter.BayerBG:
+                            nparr = cv2.cvtColor(nparr, cv2.COLOR_BAYER_BG2RGB)
+
+                    # Resize image and store to self._image.
+                    nparr = cv2.resize(nparr, None,
+                                       fx=DISPLAY_RESIZE_FACTOR,
+                                       fy=DISPLAY_RESIZE_FACTOR)
+                    self._lock.acquire()
+                    self._image = nparr
+                    self._lock.release()
