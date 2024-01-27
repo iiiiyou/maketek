275ffcea9e8523ed57159c24de2e016c5ead7445
maketk <maktek@i4u.dev>
2024-01-27 15:50:03 +0900

    20240127-004

--- omron_camera_harvesters/불량검출.bat
+++ omron_camera_harvesters/불량검출.bat
@@ -1,5 +1,5 @@
 REM @echo off
 call C:\workspace\maketek\.venv\Scripts\activate
 cd C:\workspace\maketek\omron_camera_harvesters
-python predict_omron_send_count_modbus_test.py
+python predict_omron_send_good_V02.py
 pause
